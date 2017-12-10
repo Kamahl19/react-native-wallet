b91eada537d9c72fd8054d2f987d48f057692873
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2017-12-11 00:59:59 +0100

    show active wallet name, coin and network

--- src/app/navigators/drawer/CustomDrawer.js
+++ src/app/navigators/drawer/CustomDrawer.js
@@ -5,27 +5,33 @@ import { DrawerItems } from 'react-navigation';
 import { connect } from 'react-redux';
 
 import { logout, selectUserEmail } from '../../../features/auth/ducks';
+import { selectActiveWallet } from '../../../features/wallet/ducks';
 import { View } from '../../../common/components';
 import DrawerItem from './DrawerItem';
 
 const mapStateToProps = state => ({
   email: selectUserEmail(state),
+  activeWallet: selectActiveWallet(state),
 });
 
 const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(logout()),
 });
 
-const CustomDrawer = ({ email, logout, ...rest }) => (
+const CustomDrawer = ({ email, activeWallet, logout, ...rest }) => (
   <View style={styles.component}>
     <DrawerItem label={`Hi ${email}`} />
     <DrawerItems {...rest} />
+    {activeWallet && <DrawerItem label={`Network: ${activeWallet.network}`} />}
+    {activeWallet && <DrawerItem label={`Coin: ${activeWallet.coin}`} />}
+    {activeWallet && <DrawerItem label={`Wallet Name: ${activeWallet.walletName}`} />}
     <DrawerItem label="Logout" onPress={logout} />
   </View>
 );
 
 CustomDrawer.propTypes = {
   email: PropTypes.string.isRequired,
+  activeWallet: PropTypes.object,
   logout: PropTypes.func.isRequired,
 };
 
