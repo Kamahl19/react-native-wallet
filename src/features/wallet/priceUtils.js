f8716993e7709e0f9f38b43260b05b785c7e099b
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2017-12-21 14:28:04 +0100

    fetch BCH price

--- src/features/wallet/priceUtils.js
+++ src/features/wallet/priceUtils.js
@@ -1,5 +1,5 @@
 import cryptocompare from 'cryptocompare';
 
 export async function getPrices() {
-  return await cryptocompare.priceMulti(['BTC', 'ETH'], 'USD');
+  return await cryptocompare.priceMulti(['BTC', 'BCH'], 'USD');
 }
