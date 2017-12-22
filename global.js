1cdcaeaad85899034cd788fb2b570f9f74a5532c
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2017-12-22 13:19:44 +0100

    sed NODE_ENV

--- global.js
+++ global.js
@@ -1,3 +1,4 @@
 // Inject node globals into React Native global scope.
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
+global.process.env.NODE_ENV = __DEV__ ? 'development' : 'production';
