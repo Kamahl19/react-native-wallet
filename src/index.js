8360410c30d3b86c015f1400a659527c70d20ae2
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2017-12-23 11:52:24 +0100

    update todo

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ if (Platform.OS === 'ios') {
 // }, 3000);
 
 // TODO
+// rozdelit wallet na btc a wallet
+// show balance in USD
 // BUG in bitcore-lib https://github.com/bitpay/bitcore-lib/issues/184
 // TEST https://github.com/tradle/rn-nodeify
 // Add import https://github.com/bitpay/bitcore-wallet/blob/master/bin/wallet-import
