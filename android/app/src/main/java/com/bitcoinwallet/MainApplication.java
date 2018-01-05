085fe7c1a5c406ac5bf4d4b772869f2139190592
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2018-01-05 14:36:43 +0100

    polyfill window.crypto

--- android/app/src/main/java/com/bitcoinwallet/MainApplication.java
+++ android/app/src/main/java/com/bitcoinwallet/MainApplication.java
@@ -3,6 +3,7 @@ package com.bitcoinwallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.bitgo.randombytes.RandomBytesPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RandomBytesPackage(),
             new SvgPackage(),
             new VectorIconsPackage()
       );
--- global.js
+++ global.js
@@ -2,3 +2,25 @@
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
 global.process.env.NODE_ENV = __DEV__ ? 'development' : 'production';
+
+const { randomBytes } = require('react-native-randombytes');
+
+// implement window.getRandomValues()
+if (typeof window === 'object') {
+  if (!window.crypto) window.crypto = {};
+  if (!window.crypto.getRandomValues) {
+    window.crypto.getRandomValues = function getRandomValues(arr) {
+      let orig = arr;
+      if (arr.byteLength != arr.length) {
+        // Get access to the underlying raw bytes
+        arr = new Uint8Array(arr.buffer);
+      }
+      const bytes = randomBytes(arr.length);
+      for (var i = 0; i < bytes.length; i++) {
+        arr[i] = bytes[i];
+      }
+
+      return orig;
+    };
+  }
+}
