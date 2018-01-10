7975b7d32f5a404eeea72535de30983bb12d7d59
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2018-01-10 19:54:00 +0100

    update deps and todo

--- src/index.js
+++ src/index.js
@@ -17,8 +17,19 @@ if (Platform.OS === 'ios') {
 // }, 3000);
 
 // TODO
+// Transaction is received and accredited immediatelly
+// Transactions in Transaction History are shown from newest to oldest
+// User is able to refresh Transaction History by scrolling down
+// Transaction detail holds a transaction status
+// Transaction detail holds time and date of transaction
+// Transaction detail holds transaction ID
+// Transaction detail holds address sent TO & address received FROM
+// If current address has been used, a new one is automatically generated
+// User can set amount to send in BTC, the amount is also displayed in USD
+// balance and price is being fetched nonstop - loading wheel keeps spinning without ever disappearing
 // parse btc amount (,.)
 // limit number of decimal places in btc amount to 8
+
 // format USD with https://github.com/ExodusMovement/format-currency
 // format BTC with https://github.com/ExodusMovement/number-unit
 // BUG in bitcore-lib https://github.com/bitpay/bitcore-lib/issues/184
