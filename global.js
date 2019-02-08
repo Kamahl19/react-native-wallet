0a98112b108de39050f980e981bf720ef87b3e4b
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2019-02-08 15:37:20 +0100

    nodejs global polyfill

--- global.js
+++ global.js
@@ -3,6 +3,14 @@ global.Buffer = require('buffer').Buffer;
 global.process = require('process');
 global.process.env.NODE_ENV = __DEV__ ? 'development' : 'production';
 
+// Needed so that 'stream-http' chooses the right default protocol.
+global.location = {
+  protocol: 'file:',
+};
+
+// Some modules expect userAgent to be a string
+global.navigator.userAgent = 'React Native';
+
 const { randomBytes } = require('react-native-randombytes');
 
 // implement window.getRandomValues()
