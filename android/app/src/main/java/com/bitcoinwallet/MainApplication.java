3906887057de77ae88276b227b75050e4036956a
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2019-02-08 17:44:25 +0100

    remove icons

--- android/app/src/main/java/com/bitcoinwallet/MainApplication.java
+++ android/app/src/main/java/com/bitcoinwallet/MainApplication.java
@@ -6,7 +6,6 @@ import com.facebook.react.ReactApplication;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.horcrux.svg.SvgPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -29,8 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new RCTCameraPackage(),
             new RandomBytesPackage(),
-            new SvgPackage(),
-            new VectorIconsPackage()
+            new SvgPackage()
       );
     }
 
