607c40a23c228582b6d346fb0595e4119c5806b8
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2019-02-08 18:23:48 +0100

    upgrade deps

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { AppRegistry, Platform } from 'react-native';
-import KeyboardManager from 'react-native-keyboard-manager';
+import { AppRegistry } from 'react-native';
 
 import Root from './app/containers/Root';
 
 AppRegistry.registerComponent('bitcoinwallet', () => Root);
 
-if (Platform.OS === 'ios') {
-  KeyboardManager.setEnableDebugging(__DEV__);
-  KeyboardManager.setEnable(true);
-  KeyboardManager.setEnableAutoToolbar(false);
-}
+// TODO
+// - rename app to rncryptowallet
+// - init new RN 0.53
+// - upgrade based on paynut deps
+// - https://github.com/Kamahl19/react-native-wallet/commit/dd2699203125df5a44cfbd7ede530a8f3317a226
+// - https://github.com/Kamahl19/react-native-wallet/commit/6879e8e13ccb7195a1a468e9b9305a5246bd5906
