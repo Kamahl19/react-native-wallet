fed8e3e7d75c8eaac15d4949dd5d4f25c7a75f16
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2019-02-11 20:10:00 +0100

    custom button and input

--- src/common/components/index.js
+++ src/common/components/index.js
@@ -1,15 +1,14 @@
 export {
   ActivityIndicator,
-  Button,
   FlatList,
   Picker,
   RefreshControl,
   Text,
-  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
 
+export { default as Button } from './Button';
 export { default as CenterView } from './CenterView';
 export { default as FormItem } from './FormItem';
 export { default as Heading } from './Heading';
@@ -17,3 +16,4 @@ export { default as OnePicker } from './OnePicker';
 export { default as Scanner } from './Scanner';
 export { default as ScreenWrapper } from './ScreenWrapper';
 export { default as SegmentedControl } from './SegmentedControl';
+export { default as TextInput } from './TextInput';
