e714466d0b371abe2cbdc0012072a1d5973f0721
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2019-02-12 10:36:51 +0100

    fix qrcode scanner

--- android/app/src/main/java/com/rncryptowallet/MainApplication.java
+++ android/app/src/main/java/com/rncryptowallet/MainApplication.java
@@ -3,9 +3,9 @@ package com.rncryptowallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.horcrux.svg.SvgPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SvgPackage(),
-            new RandomBytesPackage(),
-            new RCTCameraPackage()
+            new RNCameraPackage(),
+          new SvgPackage(),
+          new RandomBytesPackage()
       );
     }
 
